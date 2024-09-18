24ca3f7b370a4775c3629ccd118267a19548161f
Suniel <Suniel.paudel2047@gmail.com>
2024-09-17 21:09:11 -0700

    Added .cpp file.

---
+++ ClassActivities14/secretNumber.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>  
+#include <ctime>
+
+using namespace std;
+
+int main() {
+
+    srand(time(0));
+
+    
+    int secretNumber = rand() % 100 + 1;  
+    int guess = 0;
+
+    cout << "Guess the secret number between 1 and 100:" << endl;
+
+    while (guess != secretNumber) {  
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        if (guess < secretNumber) {
+            cout << "Too low! Try again." << endl;
+        }
+        else if (guess > secretNumber) {
+            cout << "Too high! Try again." << endl;
+        }
+        else {
+            cout << "Congratulations! You guessed the secret number!" << endl;
+        }
+    }
+
+    return 0;
+}
